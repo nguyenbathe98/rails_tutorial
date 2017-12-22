50ca1cf1cd7d8bc3dfcd193ad4bff721308552e9
Nguyen Ba The <nguyenbathe98@gmail.com>
2017-12-22 18:58:08 +0700

    end signup git add .!

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,5 @@ Rails.application.routes.draw do
 
   get 'sigup', to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
 end
