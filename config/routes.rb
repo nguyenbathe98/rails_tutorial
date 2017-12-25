7d5fd8dd2121ec49d1dd1305ff7ce5d1bb06cf47
Nguyen Ba The <nguyenbathe98@gmail.com>
2017-12-25 18:04:11 +0700

    Implement ba sic logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
 	get "users/new"
@@ -11,7 +13,11 @@ Rails.application.routes.draw do
 
   root "static_pages#home"
 
-  get 'sigup', to: 'users#new'
+  get 'signup', to: 'users#new'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
 end
