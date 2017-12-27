4d55171bbcf6cd7a99577a28ffefeaef9ea20120
Nguyen Ba The <nguyenbathe98@gmail.com>
2017-12-27 18:32:36 +0700

    Update assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require /jquery/dist/jquery.js
+//= require jquery/dist/jquery.js
 //= require bootstrap
 //= require rails-ujs
 //= require turbolinks
