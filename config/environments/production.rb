a9c4da1b104f2a13f93f36f0b3e606dabd64a8c5
Nguyen Ba The <nguyenbathe98@gmail.com>
2018-01-03 20:41:24 +0700

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,11 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "sample_app_#{Rails.env}"
   config.action_mailer.perform_caching = false
-
+  config.active_record.migration_error = :page_load
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :letter_opener
+  host = 'localhost:3000'                     # Local server
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
