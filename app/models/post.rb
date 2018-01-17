bf22cfd3336605cf4cd6941d16f9da24d8458201
Nguyen Ba The <nguyenbathe98@gmail.com>
2018-01-17 21:18:43 +0700

    Edit comment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
 	self.per_page=5
+	has_many :comments
 	belongs_to :creator,
 	class_name: User.name, foreign_key: :user_id
 
--- config/routes.rb
+++ config/routes.rb
@@ -27,5 +27,5 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :posts
-  resources :comments,            only: [:create]
+  resources :comments,            only: [:create, :edit, :update]
 end
